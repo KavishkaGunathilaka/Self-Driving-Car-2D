08c3917fde558c9146a0271f6280923dd95ba9a7
KavishkaGunathilaka <gihanthakavishka@gmail.com>
2022-07-21 20:19:49 +0530

    Car can turn left and right

--- car.js
+++ car.js
@@ -7,8 +7,9 @@ class Car{
 
         this.speed=0;
         this.acceleration=0.1;
-        this.maxSpeed=3;
+        this.maxSpeed=2;
         this.friction=0.05;
+        this.angle=0;
 
         this.controls=new Controls();
     }
@@ -45,17 +46,41 @@ class Car{
             this.speed=0;
         }
 
+        // flip left and right controls when car is reversing
+        if (this.speed != 0){
+            const flip = this.speed > 0 ? 1 : -1;
+            
+            if(this.controls.left){
+                this.angle += flip*0.03;
+            }
+    
+            if(this.controls.right){
+                this.angle -= flip*0.03;
+            }
+
+        }
+
+        this.x -= Math.sin(this.angle)*this.speed;
+        this.y -= Math.cos(this.angle)*this.speed;
+
         this.y -= this.speed;
     }
 
     draw(ctx){
+        ctx.save();
+        ctx.translate(this.x, this.y);
+        ctx.rotate(-this.angle);
+
         ctx.beginPath();
         ctx.rect(
-            this.x-this.width/2,
-            this.y-this.height/2,
+            -this.width/2,
+            -this.height/2,
             this.width,
             this.height
         );
+
         ctx.fill();
+
+        ctx.restore();
     }
 }
